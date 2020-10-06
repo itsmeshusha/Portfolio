67247641380e4dc3e0c4900411255d20f5672426
Alexandra <alexbushueva27@gmail.com>
2020-10-06 17:25:02 +0300

    Updates

---
+++ precache-manifest.0c2ff54104549ce772c69ef4ca0b7947.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df107593e8ca9ef6c2b5851741e3379b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "022ba22276c6848d8e7e",
+    "url": "/Portfolio/static/css/main.507b7ab5.chunk.css"
+  },
+  {
+    "revision": "5c0672c90b86a3b41aeb",
+    "url": "/Portfolio/static/js/2.e5a317cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.e5a317cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "022ba22276c6848d8e7e",
+    "url": "/Portfolio/static/js/main.e807b812.chunk.js"
+  },
+  {
+    "revision": "4ede5ce3c4791437294c",
+    "url": "/Portfolio/static/js/runtime-main.32b63bf9.js"
+  },
+  {
+    "revision": "8fb3c2566c07d478531eb475a84f6235",
+    "url": "/Portfolio/static/media/MyPhoto.8fb3c256.jpg"
+  }
+]);
