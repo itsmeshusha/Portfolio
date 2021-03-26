24dbb58222bd576fc3afee34dff745b0b8bc25ce
Alexandra <alexbushueva27@gmail.com>
2021-03-26 20:10:18 +0300

    contact form updated

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import s from './Contacts.module.css';
 import styleContainer from '../Common/Styles/Container.module.css';
+import Title from "../Common/Components/Title/Title";
 
 function Contacts() {
     return (
         <div className={s.contacts}>
             <div className={`${styleContainer.container} ${s.contactsContainer}`}>
-                <h2 className={s.title}>My Contacts</h2>
+                <Title title={"Contact me"}/>
                 
                     <form className={s.forms}>
                         <input className={s.input} type="text" placeholder="Your fullname" />
