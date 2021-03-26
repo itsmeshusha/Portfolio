3ca772e978313370901322b2c850df9a2ec5cd88
Alexandra <alexbushueva27@gmail.com>
2021-03-26 18:14:45 +0300

    my projects block is done

--- src/Projects/Project/Project.tsx
+++ src/Projects/Project/Project.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import s from './Project.module.css'
+import {ImageType} from "../Projects";
 
 type PropsType = {
     title: string
     description: string
+    image: ImageType
 }
 
 function Project(props: PropsType) {
     return (
-        
-            <div className={s.project}>
-            <div className={s.icon}>
+
+        <div className={s.project}>
+            <div className={s.icon} style={props.image}>
                 <a className={s.viewBtn}>View more</a>
             </div>
-            <h3>{props.title}</h3>
-            <span className={s.description}>
+            <div className={s.projectInfo}>
+                <h3 className={s.projectTitle}>{props.title}</h3>
+                <span className={s.description}>
                 {props.description}
             </span>
+            </div>
         </div>
     )
 }
--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -2,21 +2,30 @@ import React from 'react';
 import s from './Projects.module.css';
 import styleContainer from '../Common/Styles/Container.module.css'
 import Project from './Project/Project';
-import Title from "../Common/Components/Title/Title";
+import Title from '../Common/Components/Title/Title';
+import todo from '../Assets/images/todolist.jpg';
+import owl from '../Assets/images/moscowl.jpg'
 
-
+export type ImageType = {
+    backgroundImage: string
+}
 function Projects() {
+    const todolist: ImageType  = {
+        backgroundImage: `url(${todo})`
+    }
+    const moscowl: ImageType = {
+        backgroundImage: `url(${owl})`
+    }
+
     return (
         <div className={s.projectsBlock}>
             <div className={`${styleContainer.container} ${s.projectsContainer}`}>
                 <Title title={"My projects"} />
                 <div className={s.projects}>
-                    <Project title={"Social Network"} 
+                    <Project image={todolist} title={"Todo List"}
                             description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"} />
-                    <Project title={"Todo List"} 
+                    <Project image={moscowl} title={"Moscow MTG Legacy"}
                             description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"} />
-                    <Project title={"Counter"} 
-                            description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"} />        
 
                 </div>
             </div>
