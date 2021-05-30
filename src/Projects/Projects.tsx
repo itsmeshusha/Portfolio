e994f5ff2981f872b3db1cbc0c0c6b565560dc86
Alexandra <alexbushueva27@gmail.com>
2021-05-30 19:08:48 +0300

    Add new projects

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -4,7 +4,9 @@ import styleContainer from '../Common/Styles/Container.module.css'
 import Project from './Project/Project';
 import Title from '../Common/Components/Title/Title';
 import todo from '../Assets/images/todolist.jpg';
-import owl from '../Assets/images/moscowl.jpg'
+import owl from '../Assets/images/moscowl.jpg';
+import photogallery from '../Assets/images/photogallery.jpg'
+import cardsforlearn from '../Assets/images/cards.jpg'
 // @ts-ignore
 import Fade from 'react-reveal/Fade';
 
@@ -20,6 +22,12 @@ function Projects() {
     const moscowl: ImageType = {
         backgroundImage: `url(${owl})`
     }
+    const photos: ImageType = {
+        backgroundImage: `url(${photogallery})`
+    }
+    const cards: ImageType = {
+        backgroundImage: `url(${cardsforlearn})`
+    }
 
     return (
         <div className={s.projectsBlock} id={'projects'}>
@@ -30,9 +38,13 @@ function Projects() {
                 <Fade right>
                 <div className={s.projects}>
                     <Project image={todolist} title={"Todo List"} href={'https://github.com/itsmeshusha/TodoList'}
-                            description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"} />
+                            description={"Todolist for your daily routine. You can create lot of todolists for different needs."} />
                     <Project image={moscowl} title={"Moscow MTG Legacy"} href={'https://github.com/itsmeshusha/moscow-owl'}
-                            description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"} />
+                            description={"App for Magic the Gathering legacy tournaments."} />
+                    <Project image={photos} title={"Photo Gallery"} href={'https://github.com/itsmeshusha/firebase-storage'}
+                             description={"App for uploading photos and displaying them."} />
+                    <Project image={cards} title={"Cards for studying"} href={'https://github.com/itsmeshusha/IncFriday'}
+                             description={"App for you to study."} />
 
                 </div>
                 </Fade>
